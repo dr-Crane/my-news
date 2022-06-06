353070504bd5fa20ccfac7e8259a683197ced310
Bekzat <bekzat.kanatbekulu@gmail.com>
2022-06-06 14:58:38 +0700

    Edited navbar buttons Apart from it added AuthenticationController.cs

---
+++ MyNews/Controllers/AuthenticationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyNews.Models;
+using System.Diagnostics;
+
+
+namespace MyNews.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
