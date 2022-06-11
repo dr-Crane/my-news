9c44ce900afc92b0ec56319d68058ec5aa9f0554
Bekzat <bekzat.kanatbekulu@gmail.com>
2022-06-11 10:25:11 +0700

    Added base NewsModel description

---
+++ MyNews/Models/NewsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MyNews.Models
+{
+    public class NewsModel
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Content { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
