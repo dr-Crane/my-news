c6688b2309ccebf9ecb6a98efb4ff7843032bf5d
Bekzat <bekzat.kanatbekulu@gmail.com>
2022-06-11 13:10:34 +0700

    Added: -ApplicationDbContext -ApplicationRoleNames -Role -RoleType -User -UserRole files Connected Identity package

---
+++ MyNews/Storage/RoleType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyNews.Storage
+{
+    public enum RoleType
+    {
+        [Display(Name = ApplicationRoleNames.Moderator)]
+        Administrator,
+        [Display(Name = ApplicationRoleNames.User)]
+        User
+    }
+}
---
+++ MyNews/Storage/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyNews.Storage
+{
+    public class User : IdentityUser<Guid>
+    {
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
