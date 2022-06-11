fb52aa7379894baabc9f50fe8b82369c128e6bee
Bekzat <bekzat.kanatbekulu@gmail.com>
2022-06-11 15:34:24 +0700

    Added LoginViewModel

---
+++ MyNews/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyNews.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email обязателен для заполнения")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пароль обязателен для заполнения")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+    }
+}
